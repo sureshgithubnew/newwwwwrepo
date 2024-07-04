369080c6fbd40708c787545d02b9ef9c10ce7bab
sureshgitnew <53163973+sureshgitnew@users.noreply.github.com>
2024-07-04 13:04:03 +0530

    Create Hello1.java

---
+++ Hello1.java
@@ -0,0 +1,10 @@
+public class SimpleMethod {
+    public static void main(String[] args) {
+        int result = add(10, 5);
+        System.out.println("Sum: " + result);
+    }
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+}
